c2dce72c403467f3b31cb8c5221c44e5326e5ea6
jescomarketing <jescomarketing@MacBook-Pro.local>
2016-11-20 15:49:50 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+// Create our app
+const app = express()
+
+app.use(express.static('public'))
+
+app.listen(3000, () => {
+  console.log('Express server is up on port 3000')
+})
